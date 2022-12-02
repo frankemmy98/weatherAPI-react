4a7c03cba7bec9423a008f679709d054f3cdcf7c
frankemmy98 <emmyfrank349@gmail.com>
2022-12-02 17:09:57 +0100

    updated the singleWeather component

--- src/components/SingleWeather.js
+++ src/components/SingleWeather.js
@@ -8,47 +8,42 @@ function SingleWeather(weatherData) {
   const imageUrl = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
   const cityName = weatherData.name;
 
+  const cities = [
+    {
+      id: 1,
+      title: "lagos",
+    },
+    {
+      id: 2,
+      title: "abuja",
+    },
+    {
+      id: 3,
+      title: "kano",
+    },
+  ];
+
   return (
     <>
-      <div className="images-container">
-        <div className="image-box">
-          <div className="bcg">
-            <h2>{temp}℃</h2>
-            <h2>{weatherMain}</h2>
-            <img src={imageUrl} alt="" />
-            <h1 className="city">{cityName}</h1>
-          </div>
-          <div className="content">
-            <p className="title">{weatherDescription}</p>
-            <h4 className="temp">{temp}℃</h4>
-          </div>
-        </div>
+      <div className="images__container">
+        {cities.map((city) => {
+          const { id, title } = city;
 
-        <div className="image-box">
-          <div className="bcg">
-            <h2>{temp}℃</h2>
-            <h2>Clouds</h2>
-            <img src={imageUrl} alt="" />
-            <h1 className="city">{cityName}</h1>
-          </div>
-          <div className="content">
-            <p className="title">{weatherDescription}</p>
-            <h4 className="temp">{temp}℃</h4>
-          </div>
-        </div>
-
-        <div className="image-box">
-          <div className="bcg">
-            <h2>{temp}℃</h2>
-            <h2>Clouds</h2>
-            <img src={imageUrl} alt="" />
-            <h1 className="city">{cityName}</h1>
-          </div>
-          <div className="content">
-            <p className="title">scattered clouds</p>
-            <h4 className="temp">{temp}℃</h4>
-          </div>
-        </div>
+          return (
+            <div key={id} className="image__box">
+              <div className="bcg">
+                <h2>{temp}℃</h2>
+                <h2>{weatherMain}</h2>
+                <img src={imageUrl} alt="" />
+                <h1 className="city">{cityName}</h1>
+              </div>
+              <div className="content">
+                <p className="title">{weatherDescription}</p>
+                <h4 className="temp">{temp}℃</h4>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </>
   );
